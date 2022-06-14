955d5c4695220cc2b9736452a919e740a0fa0d61
buddy <81221429+taehwan0@users.noreply.github.com>
2022-06-14 23:50:41 +0900

    Security 4

--- src/main/java/com/cos/security/auth/PrincipalDetailsService.java
+++ src/main/java/com/cos/security/auth/PrincipalDetailsService.java
@@ -18,7 +18,7 @@ public class PrincipalDetailsService implements UserDetailsService {
 
     // 폼에서 name="username" 과 매개변수 String username 매칭이 되어 있어야 함
     // 파라미터 바꾸고 싶으면 SecurityConfig 에서 parameter name 세팅 필요
-    // 시큐리티 session = Authentication = UserDetails
+    // 시큐리티 session(Authentication(UserDetails))) 의 구조
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User userEntity = userRepository.findByUsername(username).orElseThrow(() -> null);
